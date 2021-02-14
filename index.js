f1b107ecbc7ac031b8130924deb5557ffff8d37f
schinken <mail+github@schinken.io>
2021-02-14 15:14:38 +0100

    Add basic logging with timestamp

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const mqtt = require('mqtt');
 const config = require('./config/default');
 const SmartmeterObis = require('smartmeter-obis');
 
+function log(severity, message) {
+    const timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    console.log(`[${timestamp}] ${severity} - ${message}`);
+}
+
 const mqttClient  = mqtt.connect('mqtt://' + config.mqtt.server, {
     will: {
         topic: config.mqtt.topic + '/state',
@@ -17,14 +22,16 @@ var smTransport = SmartmeterObis.init({
 }, (error, result) => {
     if (error) {
         mqttClient.publish(config.mqtt.topic + '/state', 'disconnected', config.mqtt.options);
-        return console.error(error);
+        return log('ERR', error.message);
     }
 
-    console.log('Received new SML result');
+    log('INFO', 'Received SML message with ' + Object.keys(result).length + ' topics');
     for (var id in result) {
         mqttClient.publish(config.mqtt.topic + '/' + result[id].idToString(), result[id].valueToString(), config.mqtt.options);
     }
 });
 
 mqttClient.publish(config.mqtt.topic + '/state', 'connected', config.mqtt.options);
+log('INFO', 'Waiting for SML messages on ' + config.serialDevice);
+
 smTransport.process();
