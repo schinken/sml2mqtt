5455e0ba6d3753840e1e21f8939f24042ee275fc
schinken <mail+github@schinken.io>
2021-02-14 11:16:25 +0100

    initial commit

---
+++ config/example.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+    serialDevice: '/dev/smartmeter/n',
+    mqtt: {
+        server: 'mqtt.server.org',
+        baseTopic: 'infrastructure/smartmeter/house'
+    }
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+const mqtt = require('mqtt');
+const config = require('./config/default');
+const SmartmeterObis = require('smartmeter-obis');
+
+const mqttClient  = mqtt.connect('mqtt://' + config.mqtt.server);
+
+var smTransport = SmartmeterObis.init({
+    protocol: 'SmlProtocol',
+    transport: 'SerialResponseTransport',
+    transportSerialPort: config.serialDevice
+}, (error, result) => {
+    if (error) {
+        return console.error(error);
+    }
+
+    for (var id in result) {
+        mqttClient.publish(config.mqtt.topic + '/' + result[id].idToString(), result[id].valueToString());
+    }
+});
+
+smTransport.process();
